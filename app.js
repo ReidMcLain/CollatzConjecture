619e2e0ecf9ba514452500bcf63916cfe4e801cc
Reid McLain <97499183+ReidMcLain@users.noreply.github.com>
2023-06-28 11:58:03 -0400

    Update app.js to display on html instead

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-const done = length => console.log(`Collatz conjecture proven true in ${length} steps`);
+const done = length => {
+  const outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = `Collatz conjecture proven true in ${length} steps`;
+};
+
 let counter = 0;
 counter++;
 
 const countdown = (num, callback) => {
-    if (num == 1) {
-        callback(counter);
+  if (num == 1) {
+    callback(counter);
+  } else {
+    counter++;
+    if (num % 2 === 0) {
+      setTimeout(() => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.innerHTML += num + '<br>';
+        countdown(num / 2, callback);
+      }, 100);
     } else {
-        counter++
-        if (num % 2 === 0) {
-            setTimeout(() => {
-                console.log(num);
-                countdown(num / 2, callback);
-            }, 100);
-        } else { 
-            setTimeout(() => {
-                console.log(num);
-                countdown(3 * num + 1, callback);
-            }, 100);
-        }
+      setTimeout(() => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.innerHTML += num + '<br>';
+        countdown(3 * num + 1, callback);
+      }, 100);
     }
+  }
 };
 
-countdown(103, done);
+document.getElementById('convergeBtn').addEventListener('click', () => {
+  const inputText = document.getElementById('inputNumber').value;
+  const inputNumber = parseInt(inputText);
+
+  if (!isNaN(inputNumber)) {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '';
+    countdown(inputNumber, done);
+  } else {
+    console.log('Invalid input. Please enter a valid number.');
+  }
+});
